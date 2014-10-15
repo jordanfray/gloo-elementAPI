dd8cb19beaefbd277aa425c5b341c29d297e1f9d
Jordan Fray <jofray@tangogroup.com>
2014-10-15 16:30:46 -0600

    added get ID on click

---
+++ javascriptFunctions/getIdOnClick.js
@@ -0,0 +1,5 @@
+// Select an ID, Class, or html element that you want to get the Id for when clicked
+$('some_Id_or_class').click(function() {
+    var id= $(this).attr('id');
+    // Do something with the id
+});
