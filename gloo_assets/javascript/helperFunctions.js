41cfc0dcf926190e9cb3588406983dec6d107e1c
Jordan Fray <jofray@tangogroup.com>
2014-10-15 10:23:37 -0600

    Added the gloo_assets bundle to the repo

---
+++ gloo_assets/javascript/helperFunctions.js
@@ -0,0 +1,14 @@
+// This fuction sets up the userObject with easy access to all of it's data
+function getUserObject() {
+	var user = {
+		"id":elementAPI.user().data.id,
+		"first_name":elementAPI.user().data.first_name,
+		"last_name":elementAPI.user().data.last_name,
+		"has_partner":elementAPI.user().data.has_partner,
+		"relationship_id":elementAPI.user().data.partner_relationship_id,
+		"gender":elementAPI.user().gender(),
+		"avatar":elementAPI.user().avatar()
+	}
+
+	return user;
+}
